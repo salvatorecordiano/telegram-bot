c03fe33b69840972b434e4d0acc7fe8111ec6b82
Salvatore Cordiano <salvatorecordiano@users.noreply.github.com>
2020-10-25 21:36:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,25 @@
 * @returns {object.http} The response
 */
 module.exports = async (context) => {
-  
-  const request = JSON.parse(context.http.body);
-  console.log('Request ', context.http.body);
-  
-  let body = {};
-  body.chat_id = request.message.chat.id;
-  body.text = request.message.text;
-  body.method = 'sendMessage';
-  
-  let response = {
+  console.log(`Request ${context.http.body}`);
+  try {
+    const request = JSON.parse(context.http.body);
+    const body = {
+      'chat_id': request.message.chat.id,
+      'text': request.message.text,
+      'method': 'sendMessage'
+    };    
+    return buildResponse(200, body);
+  } catch (e) {
+    console.log(`Wrong request ${e.name}: ${e.message}`);
+    return buildResponse(400, {'error': 'Bad request'});
+  }
+};
+
+const buildResponse = (statusCode, body) => {
+  return {
     headers: {'Content-Type': 'application/json'},
-    statusCode: 200,
+    statusCode: statusCode,
     body: JSON.stringify(body)
   };
-  
-  return response;
 };
